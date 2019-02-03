d72069c1f081e44d156141a13ac199f65facca17
Hans Scheuren <hans@goldwasserexchange.be>
2019-02-03 20:37:55 +0100

    gfdg

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
+    
+    
 
     return (
       <Layout>
