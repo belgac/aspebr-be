cd68b66198164d95112a394f78ca62f674492790
Hans Scheuren <hans@goldwasserexchange.be>
2019-02-04 14:19:17 +0100

    add background to preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
+  <div class="container" style={{ backgroundColor: '#FFF', height: '100%', padding: '1.5rem' }}>
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      content={widgetFor('body')}
+    />
+  </div>
 )
 
 AboutPagePreview.propTypes = {
--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
+  <div class="container" style={{ backgroundColor: '#FFF', height: '100%', padding: '1.5rem' }}>
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+    />
+  </div>
 )
 
 BlogPostPreview.propTypes = {
