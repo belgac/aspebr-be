7609d06bf9c6d9290ef8864dee380876cd415427
Hans Scheuren <hans@goldwasserexchange.be>
2019-02-04 17:14:19 +0100

    change meta title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'ASPEBr : Association des diplômés en sciences sociales, politiques, économiques, informatique et sciences humaines et sciences du travail de l’ULB',
+    description: 'ASPEBr : Association des diplômés en sciences sociales, politiques, économiques, informatique et sciences humaines et sciences du travail de l’ULB',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
