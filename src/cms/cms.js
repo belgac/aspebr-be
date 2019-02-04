2565eeae9c9be8b4637c3571d210f9e06bc63c29
Hans Scheuren <hans@goldwasserexchange.be>
2019-02-04 11:47:20 +0100

    add missing preview templates

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
+CMS.registerPreviewTemplate('home', AboutPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('activites', AboutPagePreview)
+CMS.registerPreviewTemplate('info-aspebr', AboutPagePreview)
+CMS.registerPreviewTemplate('contact', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
